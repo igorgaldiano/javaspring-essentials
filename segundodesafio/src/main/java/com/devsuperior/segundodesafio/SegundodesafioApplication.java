6f1ab2b97dd3d297a0f5a6b48b3b3a4eb0714257
igorgaldiano <igorgaldiano80@gmail.com>
2025-03-14 20:56:13 -0300

    Primeiro Comite

---
+++ segundodesafio/src/main/java/com/devsuperior/segundodesafio/SegundodesafioApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.segundodesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundodesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundodesafioApplication.class, args);
+	}
+
+}
---
+++ segundodesafio/src/test/java/com/devsuperior/segundodesafio/SegundodesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.segundodesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundodesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
